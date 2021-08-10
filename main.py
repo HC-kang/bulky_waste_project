df3b877dd3e0c84102b3a1052fcb0836e486dc2a
HC-kang <weston0713@gmail.com>
2021-08-10 21:08:34 +0900

    0810

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ pose = mpPose.Pose()
 
 #cap = cv2.VideoCapture('testVideo01.mp4')
 # cap = cv2.VideoCapture('testVideo02.mp4')
-cap = cv2.VideoCapture('/Users/heechankang/Downloads/DUMBDUMB.mp4')
+cap = cv2.VideoCapture('/Users/heechankang/Downloads/SENORITA.mp4')
 # cap = cv2.VideoCapture(1)
 pTime = 0
-out = cv2.VideoWriter('DUMBDUMB_mediapipe.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (854, 480))
+out = cv2.VideoWriter('SENORITA_mediapipe.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (854, 480))
 while cap.isOpened():
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -29,10 +29,6 @@ while cap.isOpened():
         #     cx, cy = int(lm.x * w), int(lm.y * h)
         #     cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
 
-    if not success:
-        out.release()
-        break
-
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
@@ -41,4 +37,5 @@ while cap.isOpened():
     out.write(img)
     cv2.waitKey(10)
     cv2.destroyAllWindows()
+cap.release()
 out.release()
